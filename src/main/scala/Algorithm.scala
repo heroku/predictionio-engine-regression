d15947ed3640cc482cf006a11a742b26303d7b36
Mars Hall <mars@heroku.com>
2017-03-14 17:55:41 -0700

    clean-up Algorithm

--- src/main/scala/Algorithm.scala
+++ src/main/scala/Algorithm.scala
@@ -2,7 +2,6 @@ package org.template.regression
 
 import org.apache.predictionio.controller.P2LAlgorithm
 import org.apache.predictionio.controller.Params
-import org.apache.predictionio.data.storage.BiMap
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -23,11 +22,6 @@ class Algorithm(val ap: AlgorithmParams)
   @transient lazy val logger = Logger[this.type]
 
   def train(sc: SparkContext, data: PreparedData): LinearRegressionModel = {
-
-    logger.info(s"~~~~~ AlgorithmParams ${ap}")
-
-    val labelMap = BiMap.stringDouble(data.values.map(_._1))
-
     val labeledPoints: RDD[LabeledPoint] = data.values.map {
       case (entityId, properties) =>
         LabeledPoint(properties.get[Double]("label"),
@@ -36,32 +30,10 @@ class Algorithm(val ap: AlgorithmParams)
     }
     .cache
 
-    val labels = labeledPoints.take(5).map {
-      (v) => s"${v.label}=>${v.features}"
-    }
-    logger.info(s"~~~~~ LabeledPoints: ${labels.mkString(", ")}")
-
-    val model = LinearRegressionWithSGD.train(
+    LinearRegressionWithSGD.train(
       labeledPoints,
       ap.numIterations,
       ap.stepSize)
-
-    logger.info(s"~~~~~ Model: ${model}")
-
-    val valuesAndPreds = labeledPoints.map { point =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
-    }
-
-    val preds = valuesAndPreds.take(5).map {
-      (v) => s"${v._1}=>${v._2}"
-    }
-    logger.info(s"~~~~~ Label=>Prediction: ${preds.mkString(", ")}")
-
-    val MSE = valuesAndPreds.map{case(v, p) => math.pow((v - p), 2)}.mean()
-    logger.info("~~~~~ Mean Squared Error: " + MSE)
-
-    model
   }
 
   def predict(model: LinearRegressionModel, query: Query): PredictedResult = {
